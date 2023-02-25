87ffd4236caf4234ea319809e70b1a249cb6f348
Ismail Cetin <isocetin1907@gmail.com>
2023-02-25 17:38:20 +0100

    Ep2: Modal

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <script setup>
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { ref } from "vue";
+
+import {
+  Menu,
+  MenuButton,
+  MenuItems,
+  MenuItem,
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/vue";
 import { ChevronDownIcon } from "@heroicons/vue/24/solid";
 import { PencilIcon } from "@heroicons/vue/24/outline";
 import { DocumentDuplicateIcon } from "@heroicons/vue/24/outline";
 import { ArchiveBoxIcon } from "@heroicons/vue/24/outline";
 import { XCircleIcon } from "@heroicons/vue/24/outline";
+import { XMarkIcon } from "@heroicons/vue/24/outline";
+
+const isOpen = ref(false);
+
+function closeModal() {
+  isOpen.value = false;
+}
+function openModal() {
+  isOpen.value = true;
+}
 </script>
 
 <template>
@@ -97,6 +119,80 @@ import { XCircleIcon } from "@heroicons/vue/24/outline";
           voluptate voluptatum quia cupiditate? Saepe inventore est dolores sed
           rem aspernatur accusamus dolor libero, id voluptate suscipit
           laudantium at molestias aliquid?
+        </div>
+      </div>
+      <div class="mt-4">
+        <h2 class="font-bold text-xl pt-2">Dialog (Modal)</h2>
+        <div class="mt-4">
+          <button
+            type="button"
+            @click="openModal"
+            class="flex items-center bg-blue-500 text-white rounded-md px-4 py-2"
+          >
+            Open Modal
+          </button>
+          <TransitionRoot appear :show="isOpen" as="template">
+            <Dialog as="div" @close="closeModal" class="relative z-10">
+              <TransitionChild
+                as="template"
+                enter="duration-300 ease-out"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="duration-200 ease-in"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+              >
+                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+              </TransitionChild>
+
+              <div class="fixed inset-0 overflow-y-auto">
+                <div
+                  class="flex min-h-full items-center justify-center p-4 text-center"
+                >
+                  <TransitionChild
+                    as="template"
+                    enter="duration-300 ease-out"
+                    enter-from="opacity-0 scale-95"
+                    enter-to="opacity-100 scale-100"
+                    leave="duration-200 ease-in"
+                    leave-from="opacity-100 scale-100"
+                    leave-to="opacity-0 scale-95"
+                  >
+                    <DialogPanel
+                      class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+                    >
+                      <button class="absolute top-3 right-3" @click="closeModal">
+                        <XMarkIcon class="w-5 h-5" />
+                      </button>
+                      <DialogTitle
+                        as="h3"
+                        class="text-lg font-medium leading-6 text-gray-900"
+                      >
+                        Payment successful
+                      </DialogTitle>
+                      <div class="mt-2">
+                        <p class="text-sm text-gray-500">
+                          Your payment has been successfully submitted. We’ve
+                          sent you an email with all of the details of your
+                          order.
+                        </p>
+                      </div>
+
+                      <div class="mt-4">
+                        <button
+                          type="button"
+                          class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                          @click="closeModal"
+                        >
+                          Got it, thanks!
+                        </button>
+                      </div>
+                    </DialogPanel>
+                  </TransitionChild>
+                </div>
+              </div>
+            </Dialog>
+          </TransitionRoot>
         </div>
       </div>
     </div>
